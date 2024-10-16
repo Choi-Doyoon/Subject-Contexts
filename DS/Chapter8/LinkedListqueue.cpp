36b2eac5a674798a9c36fe51cb55601a49811927
Choi Doyoon <chams021226@gmail.com>
2024-10-16 15:25:19 +0900

    주석 추가

--- DS/Chapter8/LinkedListqueue.cpp
+++ DS/Chapter8/LinkedListqueue.cpp
@@ -6,7 +6,7 @@ template<class Item>
 class queue{
     public:
     typedef size_t size_type;
-    queue(){
+    queue(){ //default constructor
         count=0;
         front_ptr=NULL;
     }
@@ -14,18 +14,18 @@ class queue{
         count=source.count;
         list_copy(source.front_ptr,front_ptr,rear_ptr);
     }
-    ~queue(){list_clear(front_ptr);}
+    ~queue(){list_clear(front_ptr);} //destructor(소멸자)
     void pop(){
         assert(!empty());
-        list_head_remove(front_ptr);
+        list_head_remove(front_ptr); //FIFO에 의해 빠질 때 머리부터 빠진다.
         --count;
     }
     void push(const Item& entry){
-        if(empty()){
+        if(empty()){ //큐가 비어있으면 앞에 삽입
             list_head_insert(front_ptr,entry);
             rear_ptr=front_ptr;
         }
-        else{
+        else{ //큐가 채워진 상태면 뒤에서 삽입
             list_insert(rear_ptr,entry);
             rear_ptr=rear_ptr->link();
         }
@@ -37,8 +37,8 @@ class queue{
         list_copy(source.front_ptr,front_ptr,rear_ptr);
         count=source.count;
     }
-    bool empty() const{return (count==0);}
-    Item front() const{
+    bool empty() const{return (count==0);} //비어있는 상태인지 확인
+    Item front() const{ 
         assert(!empty());
         return front_ptr->data();
     }
