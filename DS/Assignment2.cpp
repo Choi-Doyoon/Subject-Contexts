ebd61a8650264ca8ccbf9f32dbf81e6afc8a0d7c
Choi Doyoon <chams021226@gmail.com>
2024-11-13 22:15:33 +0900

    자료구조 2차 과제

--- DS/Assignment2.cpp
+++ DS/Assignment2.cpp
@@ -1,263 +1,252 @@
 #include <iostream>
-#include <cassert>
-#include <cstdlib>
+#include <string>
+#include <stack>
+#include <sstream>
+#include <cctype>
+#include <map>
+#include <stdexcept>
 using namespace std;
 
 template <typename T>
 class dnode {
 public:
     T data;          
-    dnode<T>* next;
-    dnode<T>* prev;  
+    dnode* next;
+    dnode* prev;  
 
-    dnode(const T& item){
-        data = item;
-        next = nullptr;
-        prev = nullptr;
-    }
+    dnode(T value) : data(value), next(nullptr), prev(nullptr) {}
 };
 
-template <typename T>
-class DoublyLinkedList {
+//Implement a ourDeque class template using a doubly-linked list(40 points)
+template<typename T>
+class ourDeque{
 private:
     dnode<T>* head;
     dnode<T>* tail;
-
+    int dequesize;
 public:
-    DoublyLinkedList(){
-        head = nullptr;
-        tail = nullptr;
-    }
+    //constructor
+    ourDeque() : head(nullptr), tail(nullptr), dequesize(0) {}
 
-    ~DoublyLinkedList() {
-        list_clear(); 
-    }
-
-    int list_length() const {
-        int length = 0;
-        dnode<T>* current = head;
-        while (current) {
-            length++;
-            current = current->next;
-        }
-        return length;
-    }
-
-    bool list_search(const T& item) const {
-        dnode<T>* current = head;
-        while (current) {
-            if (current->data == item) {return true;}
-            current = current->next;
-        }
-        return false;
-    }
-
-    dnode<T>* list_locate(const T& item) const {
-        dnode<T>* current = head;
-        while (current) {
-            if (current->data == item) {return current;}
-            current = current->next;
-        }
-        return nullptr;
-    }
-
-    void list_copy(const DoublyLinkedList<T>& other) {
-        list_clear(); 
+    //copy constructor
+    ourDeque(const ourDeque& other) : head(nullptr), tail(nullptr), dequesize(0) {
         dnode<T>* current = other.head;
-        while (current) {
-            list_tail_insert(current->data);
+        while(current){
+            push_back(current->data);
             current = current->next;
         }
     }
 
-    bool list_head_insert(const T& item) {
-        if (list_search(item)) {return false;} 
-        dnode<T>* newNode = new dnode<T>(item);
-        if (!head){
-            head = tail = newNode;
+    //destructor
+    ~ourDeque(){
+        while(!empty()){
+            pop_front();
         }
-        else{
-            newNode->next = head;
-            head->prev = newNode;
-            head = newNode;
-        }
-        return true;
     }
 
-    bool list_tail_insert(const T& item) {
-        if (list_search(item)) {return false;} 
-        dnode<T>* newNode = new dnode<T>(item);
-        if (!head) {
-            head = tail = newNode;
-        } else {
+    void push_back(T value){
+        dnode<T>* newNode = new dnode<T>(value);
+        if(tail){
             tail->next = newNode;
             newNode->prev = tail;
             tail = newNode;
         }
-        return true;
-    }
-
-    bool list_insert(dnode<T>* node, const T& item) {
-        if (list_search(item) || !node) {return false;}
-        dnode<T>* newNode = new dnode<T>(item);
-        newNode->next = node->next;
-        newNode->prev = node;
-
-        if (node->next) {
-            node->next->prev = newNode;
-        } 
-        else {
-            tail = newNode; 
+        else{
+            head = tail = newNode;
         }
-        node->next = newNode;
-        return true;
+        dequesize++;
     }
 
-    bool list_head_remove() {
-        if (!head) {return false;}
-        dnode<T>* temp = head;
-        head = head->next;
-        if (head) head->prev = nullptr;
-        delete temp;
-        return true;
-    }
-
-    bool list_tail_remove() {
-        if (!tail) {return false;}
-        dnode<T>* temp = tail;
-        tail = tail->prev;
-        if (tail) {tail->next = nullptr;}
-        else {head = nullptr;}
-        delete temp;
-        return true;
-    }
-
-    bool list_remove(dnode<T>* node) {
-        if (!node) {return false;}
-        if (node == head) {
-            return list_head_remove();
+    void push_front(T value){
+        dnode<T>* newNode = new dnode<T>(value);
+        if(head){
+            head->prev = newNode;
+            newNode->next = head;
+            head = newNode;
         }
-        if (node == tail) {
-            return list_tail_remove();
+        else{
+            head = tail = newNode;
         }
-        node->prev->next = node->next;
-        if (node->next) {
-            node->next->prev = node->prev;
+        dequesize++;
+    }
+
+    T pop_back(){
+        if(tail){
+            dnode<T>* temp = tail;
+            tail = tail->prev;
+            if(tail){
+                tail->next = nullptr;
+            }
+            else{
+                head = nullptr;
+            }
+            delete temp;
+            
+            dequesize--;
         }
-        delete node;
-        return true;
+        return 0;
     }
 
-    void list_clear() {
-        while (head) {
-            list_head_remove();
+    T pop_front(){
+        if(head){
+            dnode<T>* temp = head;
+            head = head->next;
+            if(head){
+                head->prev = nullptr;
+            }
+            else{
+                tail = nullptr;
+            }
+            delete temp;
+            dequesize--;
         }
-    }
-    
-    void show_contents() const {
-        dnode<T>* current = head;
-        while (current) {
-            cout << current->data << " ";
-            current = current->next;
-        }
-        cout << endl;
+        return 0;
     }
 
-    dnode<T>* get_head() const { return head; }
-    dnode<T>* get_tail() const { return tail; }
-};
+    bool empty() const{ return dequesize == 0;}
+    int size() const{ return dequesize;}
 
-template <typename T>
-class OurSet {
-private:
-    DoublyLinkedList<T> list;
-
-public:
-    OurSet() = default;
-
-    OurSet(const OurSet<T>& other) {
-        dnode<T>* current = other.list.get_head();
-        while (current) {
-            list.list_tail_insert(current->data);
-            current = current->next;
-        }
-    }
-
-    ~OurSet() {}
-
-    bool insert(const T& item) {
-        cout << "Before insert: "<<endl;
-        show_contents();
-        bool result = list.list_tail_insert(item);
-        cout << "After insert " << item << endl;
-        show_contents();
-        return result;
-    }
-
-    bool erase(const T& item) {
-        cout << "Before erase: ";
-        show_contents();
-        dnode<T>* nodeToRemove = list.list_locate(item);
-        if (nodeToRemove) {
-            list.list_remove(nodeToRemove);
-            cout << "After erase " << item << ": ";
-            show_contents();
-            return true;
-        }
-        cout << "Item " << item << " not found for erase." << endl;
-        return false; 
-    }
-
-    int count(const T& item) const {
-        return list.list_search(item) ? 1 : 0; 
-    }
-
-    OurSet<T>& operator=(const OurSet<T>& other) {
-        if (this != &other) {
-            list.list_clear(); 
-            dnode<T>* current = other.list.get_head();
-            while (current) {
-                list.list_tail_insert(current->data);
+    ourDeque<T>& operator =(const ourDeque<T>& other){
+        if(this != &other){
+            while(!empty()){
+                pop_front();
+            }
+            dnode<T>* current = other.head;
+            while(current){
+                push_back(current->data);
                 current = current->next;
             }
         }
         return *this;
     }
 
-    OurSet<T>& operator+=(const OurSet<T>& other) {
-        dnode<T>* current = other.list.get_head();
-        while (current) {
-            this->insert(current->data); 
-            current = current->next;
-        }
-        return *this;
+    T front() const{return head ? head->data : throw runtime_error("Deque is empty");}
+    T back() const{return tail ? tail->data : throw runtime_error("Deque is empty");}
+};
+
+//Implement a class evaluator that reads an infix expression from the user, converts it to a postfix expression, and evaluates the postfix expression(45 points)
+class evaluator {
+public:
+    string read_expression() {
+        cout << "Enter an infix expression: ";
+        string expression;
+        getline(cin, expression);
+        return expression;
     }
 
-    OurSet<T> operator+(const OurSet<T>& other) const {
-        OurSet<T> result = *this; 
-        result += other; 
-        return result;
-    }
+    string convert_to_postfix(const string& infix) {
+        string postfix;
+        ourDeque<char> operators;
 
-    bool operator==(const OurSet<T>& other) const {
-        dnode<T>* current = list.get_head();
-        while (current) {
-            if (!other.list.list_search(current->data)) {
-                return false;
+        for (size_t i = 0; i < infix.length(); ++i) {
+            char token = infix[i];
+
+            if (isdigit(token) || token == '.') {
+                postfix += token;
+                while (i + 1 < infix.length() && (isdigit(infix[i + 1]) || infix[i + 1] == '.')) {
+                    postfix += infix[++i];
+                }
+                postfix += " ";
+            } 
+            else if (isalpha(token)) {
+                postfix += token;
+                postfix += " ";
+            } 
+            else if (token == '(') {
+                operators.push_back(token);
+            } 
+            else if (token == ')') {
+                while (!operators.empty() && operators.back() != '(') {
+                    postfix += operators.back();
+                    postfix += " ";
+                    operators.pop_back();
+                }
+                operators.pop_back();
+            } 
+            else {
+                while (!operators.empty() && precedence(operators.back()) >= precedence(token)) {
+                    postfix += operators.back();
+                    postfix += " ";
+                    operators.pop_back();
+                }
+                operators.push_back(token);
             }
-            current = current->next;
         }
-        current = other.list.get_head();
-        while (current) {
-            if (!list.list_search(current->data)) {
-                return false;
-            }
-            current = current->next;
+
+        while (!operators.empty()) {
+            postfix += operators.back();
+            postfix += " ";
+            operators.pop_back();
         }
-        return true;
+
+        return postfix;
     }
 
-    bool operator!=(const OurSet<T>& other) const {
-        return !(*this == other);
+    double evaluate_postfix(const string& postfix) {
+        ourDeque<double> operands;
+        istringstream iss(postfix);
+        string token;
+        map<string, double> variables;
+
+        while (iss >> token) {
+            if (isdigit(token[0]) || (token[0] == '.' && token.size() > 1)) {
+                operands.push_back(stod(token));
+            } 
+            else if (isalpha(token[0])) {
+                if (variables.find(token) == variables.end()) {
+                    cout << "Enter value for " << token << ": ";
+                    double value;
+                    cin >> value;
+                    variables[token] = value;
+                }
+                operands.push_back(variables[token]);
+            } 
+            else {
+                double right = operands.back(); operands.pop_back();
+                double left = operands.back(); operands.pop_back();
+                operands.push_back(apply_operator(left, right, token[0]));
+            }
+        }
+
+        return operands.back();
+    }
+
+private:
+    int precedence(char op) {
+        if (op == '+' || op == '-') return 1;
+        if (op == '*' || op == '/') return 2;
+        return 0;
+    }
+
+    double apply_operator(double left, double right, char op) {
+        switch (op) {
+            case '+': return left + right;
+            case '-': return left - right;
+            case '*': return left * right;
+            case '/': return left / right;
+            default: throw runtime_error("Invalid operator");
+        }
     }
 };
+
+//main function(15 points)
+int main() {
+    evaluator evaluator;
+    string choice;
+
+    do {
+        string infix = evaluator.read_expression();
+        string postfix = evaluator.convert_to_postfix(infix);
+        double result = evaluator.evaluate_postfix(postfix);
+
+        cout << "Infix expression: " << infix << endl;
+        cout << "Postfix expression: " << postfix << endl;
+        cout << "Evaluation result: " << result << endl;
+
+        cout << "Do you want to evaluate another expression? (yes/no): ";
+        cin >> choice;
+        cin.ignore();
+    } while (choice == "yes");
+
+    return 0;
+}
