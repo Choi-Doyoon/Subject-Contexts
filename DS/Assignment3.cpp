3ce6f42be7f26a2ddf9b4f6a3536fc12ea5e1d7a
Choi-Doyoon <chams021226@kw.ac.kr>
2024-12-15 02:26:09 +0900

    70%

--- DS/Assignment3.cpp
+++ DS/Assignment3.cpp
@@ -2,285 +2,275 @@
 #include <vector>
 #include <algorithm>
 #include <queue>
+using namespace std;
 
-const int MINIMUM = 1;
-const int MAXIMUM = 2 * MINIMUM;
-
+template<typename T>
 class BTreeNode {
 public:
-    std::vector<int> keys;
-    std::vector<BTreeNode*> children;
-    bool isLeaf;
+    BTreeNode* parent;
+    vector<BTreeNode*> children;
+    vector<T> dataset;
 
-    BTreeNode(bool leaf) : isLeaf(leaf) {}
+    BTreeNode(BTreeNode* init_parent = nullptr)
+        : parent(init_parent), children(), dataset() {
+    }
+
+    bool is_leaf() const {
+        return children.empty();
+    }
 };
 
-class BTree {
+template<typename T>
+class Bag {
 private:
-    BTreeNode* root;
+    const int MINIMUM;
+    BTreeNode<T>* root_ptr;
 
-    // 노드에 키 삽입 (재귀적 호출)
-    void loose_insert(BTreeNode* node, int key) {
-        // 현재 노드의 적절한 삽입 위치를 찾기
-        size_t i = 0;
-        while (i < node->keys.size() && node->keys[i] < key) {
-            ++i;
-        }
+    void fix_shortage(BTreeNode<T>* node) {
+        if (!node || node->dataset.size() >= MINIMUM) return;
 
-        // 중복된 키는 삽입하지 않음
-        if (i < node->keys.size() && node->keys[i] == key) {
-            return; // 이미 존재하는 키
-        }
-
-        // 리프 노드인 경우, 키를 바로 삽입
-        if (node->isLeaf) {
-            node->keys.insert(node->keys.begin() + i, key);
-        }
-        else {
-            // 내부 노드인 경우, 자식 노드로 재귀 호출
-            loose_insert(node->children[i], key);
-
-            // 자식 노드가 초과된 경우, 노드 분할 처리
-            if (node->children[i]->keys.size() > MAXIMUM) {
-                fix_excess(node, i);
+        if (node == root_ptr && root_ptr->dataset.empty()) {
+            if (!root_ptr->is_leaf()) {
+                BTreeNode<T>* new_root = root_ptr->children[0];
+                new_root->parent = nullptr;
+                delete root_ptr;
+                root_ptr = new_root;
             }
-        }
-    }
-
-    // 초과된 노드 분할
-    void fix_excess(BTreeNode* parent, size_t index) {
-        BTreeNode* child = parent->children[index];
-        BTreeNode* sibling = new BTreeNode(child->isLeaf);
-
-        // 중간 값을 부모로 올리고, 나머지는 새로운 형제 노드로 이동
-        sibling->keys.assign(child->keys.begin() + MINIMUM + 1, child->keys.end());
-        int middleKey = child->keys[MINIMUM];
-        child->keys.resize(MINIMUM);
-
-        // 리프가 아니면, 자식 포인터도 이동
-        if (!child->isLeaf) {
-            sibling->children.assign(child->children.begin() + MINIMUM + 1, child->children.end());
-            child->children.resize(MINIMUM + 1);
-        }
-
-        // 부모 노드에 중간 값을 삽입하고, 형제 노드를 연결
-        parent->keys.insert(parent->keys.begin() + index, middleKey);
-        parent->children.insert(parent->children.begin() + index + 1, sibling);
-    }
-
-    bool loose_erase(BTreeNode* node, int key) {
-        size_t i = 0;
-        while (i < node->keys.size() && node->keys[i] < key) {
-            ++i;
-        }
-
-        if (i < node->keys.size() && node->keys[i] == key) {
-            if (node->isLeaf) {
-                node->keys.erase(node->keys.begin() + i);
-                return true;
-            }
-            else {
-                node->keys[i] = remove_biggest(node->children[i]);
-                if (node->children[i]->keys.size() < MINIMUM) {
-                    fix_shortage(node, i);
-                }
-                return true;
-            }
-        }
-
-        if (node->isLeaf) {
-            return false; // Key not found
-        }
-
-        bool result = loose_erase(node->children[i], key);
-        if (node->children[i]->keys.size() < MINIMUM) {
-            fix_shortage(node, i);
-        }
-
-        return result;
-    }
-
-    int remove_biggest(BTreeNode* node) {
-        if (node->isLeaf) {
-            int biggest = node->keys.back();
-            node->keys.pop_back();
-            return biggest;
-        }
-
-        int result = remove_biggest(node->children.back());
-        if (node->children.back()->keys.size() < MINIMUM) {
-            fix_shortage(node, node->children.size() - 1);
-        }
-
-        return result;
-    }
-
-    void fix_shortage(BTreeNode* parent, size_t index) {
-        if (index > 0 && parent->children[index - 1]->keys.size() > MINIMUM) {
-            BTreeNode* leftSibling = parent->children[index - 1];
-            BTreeNode* child = parent->children[index];
-
-            child->keys.insert(child->keys.begin(), parent->keys[index - 1]);
-            parent->keys[index - 1] = leftSibling->keys.back();
-            leftSibling->keys.pop_back();
-
-            if (!leftSibling->isLeaf) {
-                child->children.insert(child->children.begin(), leftSibling->children.back());
-                leftSibling->children.pop_back();
-            }
-        }
-        else if (index + 1 < parent->children.size() && parent->children[index + 1]->keys.size() > MINIMUM) {
-            BTreeNode* rightSibling = parent->children[index + 1];
-            BTreeNode* child = parent->children[index];
-
-            child->keys.push_back(parent->keys[index]);
-            parent->keys[index] = rightSibling->keys.front();
-            rightSibling->keys.erase(rightSibling->keys.begin());
-
-            if (!rightSibling->isLeaf) {
-                child->children.push_back(rightSibling->children.front());
-                rightSibling->children.erase(rightSibling->children.begin());
-            }
-        }
-        else {
-            if (index > 0) {
-                index--;
-            }
-
-            BTreeNode* leftChild = parent->children[index];
-            BTreeNode* rightChild = parent->children[index + 1];
-
-            leftChild->keys.push_back(parent->keys[index]);
-            leftChild->keys.insert(leftChild->keys.end(), rightChild->keys.begin(), rightChild->keys.end());
-
-            if (!rightChild->isLeaf) {
-                leftChild->children.insert(leftChild->children.end(), rightChild->children.begin(), rightChild->children.end());
-            }
-
-            parent->keys.erase(parent->keys.begin() + index);
-            parent->children.erase(parent->children.begin() + index + 1);
-            delete rightChild;
-        }
-    }
-
-public:
-    BTree() : root(nullptr) {}
-
-    // 키 삽입
-    void insert(int key) {
-        if (!root) {
-            root = new BTreeNode(true); // 루트가 없으면 새로 생성
-        }
-
-        loose_insert(root, key);
-
-        // 루트 노드가 초과된 경우, 분할 처리
-        if (root->keys.size() > MAXIMUM) {
-            BTreeNode* newRoot = new BTreeNode(false);
-            newRoot->children.push_back(root);
-            fix_excess(newRoot, 0);
-            root = newRoot; // 새로운 루트를 설정
-        }
-    }
-
-    bool erase(int key) {
-        if (!root) {
-            return false;
-        }
-
-        bool result = loose_erase(root, key);
-
-        if (root->keys.empty() && !root->isLeaf) {
-            BTreeNode* oldRoot = root;
-            root = root->children[0];
-            delete oldRoot;
-        }
-
-        return result;
-    }
-
-    int count(int key) {
-        int totalCount = 0;
-        std::queue<BTreeNode*> nodes;
-        if (root) {
-            nodes.push(root);
-        }
-
-        while (!nodes.empty()) {
-            BTreeNode* node = nodes.front();
-            nodes.pop();
-
-            totalCount += std::count(node->keys.begin(), node->keys.end(), key);
-
-            for (BTreeNode* child : node->children) {
-                nodes.push(child);
-            }
-        }
-
-        return totalCount;
-    }
-
-    void show_contents() {
-        show_recursive(root, 0);
-    }
-
-    void show_recursive(BTreeNode* node, int depth) {
-        if (!node) {
             return;
         }
 
-        for (int i = 0; i < depth; ++i) {
-            std::cout << "  ";
+        BTreeNode<T>* parent = node->parent;
+        if (!parent) return;
+
+        auto it = find(parent->children.begin(), parent->children.end(), node);
+        int index = distance(parent->children.begin(), it);
+
+        if (index > 0 && parent->children[index - 1]->dataset.size() > MINIMUM) {
+            BTreeNode<T>* left_sibling = parent->children[index - 1];
+            node->dataset.insert(node->dataset.begin(), parent->dataset[index - 1]);
+            parent->dataset[index - 1] = left_sibling->dataset.back();
+            left_sibling->dataset.pop_back();
+
+            if (!left_sibling->is_leaf()) {
+                node->children.insert(node->children.begin(), left_sibling->children.back());
+                left_sibling->children.back()->parent = node;
+                left_sibling->children.pop_back();
+            }
+        }
+        else if (index < parent->children.size() - 1 && parent->children[index + 1]->dataset.size() > MINIMUM) {
+            BTreeNode<T>* right_sibling = parent->children[index + 1];
+            node->dataset.push_back(parent->dataset[index]);
+            parent->dataset[index] = right_sibling->dataset.front();
+            right_sibling->dataset.erase(right_sibling->dataset.begin());
+
+            if (!right_sibling->is_leaf()) {
+                node->children.push_back(right_sibling->children.front());
+                right_sibling->children.front()->parent = node;
+                right_sibling->children.erase(right_sibling->children.begin());
+            }
+        }
+        else if (index > 0) {
+            BTreeNode<T>* left_sibling = parent->children[index - 1];
+            left_sibling->dataset.push_back(parent->dataset[index - 1]);
+            left_sibling->dataset.insert(left_sibling->dataset.end(), node->dataset.begin(), node->dataset.end());
+
+            if (!node->is_leaf()) {
+                left_sibling->children.insert(left_sibling->children.end(), node->children.begin(), node->children.end());
+                for (auto child : node->children) {
+                    child->parent = left_sibling;
+                }
+            }
+
+            parent->dataset.erase(parent->dataset.begin() + index - 1);
+            parent->children.erase(parent->children.begin() + index);
+            delete node;
+        }
+        else {
+            BTreeNode<T>* right_sibling = parent->children[index + 1];
+            node->dataset.push_back(parent->dataset[index]);
+            node->dataset.insert(node->dataset.end(), right_sibling->dataset.begin(), right_sibling->dataset.end());
+
+            if (!right_sibling->is_leaf()) {
+                node->children.insert(node->children.end(), right_sibling->children.begin(), right_sibling->children.end());
+                for (auto child : right_sibling->children) {
+                    child->parent = node;
+                }
+            }
+
+            parent->dataset.erase(parent->dataset.begin() + index);
+            parent->children.erase(parent->children.begin() + index + 1);
+            delete right_sibling;
         }
 
-        for (int key : node->keys) {
-            std::cout << key << " ";
+        fix_shortage(parent);
+    }
+
+    void loose_erase(BTreeNode<T>* node, T data) {
+        auto it = find(node->dataset.begin(), node->dataset.end(), data);
+
+        if (it != node->dataset.end()) {
+            if (node->is_leaf()) {
+                node->dataset.erase(it);
+            }
+            else {
+                int index = distance(node->dataset.begin(), it);
+                BTreeNode<T>* pred = node->children[index];
+                while (!pred->is_leaf()) {
+                    pred = pred->children.back();
+                }
+                swap(*it, pred->dataset.back());
+                loose_erase(pred, pred->dataset.back());
+            }
+        }
+        else if (!node->is_leaf()) {
+            if (data <= node->dataset.front()) {
+                loose_erase(node->children.front(), data);
+            }
+            else if (data > node->dataset.back()) {
+                loose_erase(node->children.back(), data);
+            }
+            else {
+                for (size_t i = 0; i < node->dataset.size(); ++i) {
+                    if (data <= node->dataset[i]) {
+                        loose_erase(node->children[i], data);
+                        break;
+                    }
+                }
+            }
         }
 
-        std::cout << "\n";
+        fix_shortage(node);
+    }
 
-        for (BTreeNode* child : node->children) {
-            show_recursive(child, depth + 1);
+    T remove_biggest(BTreeNode<T>* node) {
+        if (node->is_leaf()) {
+            T biggest = node->dataset.back();
+            node->dataset.pop_back();
+            return biggest;
         }
+        else {
+            return remove_biggest(node->children.back());
+        }
+    }
+
+    void loose_insert(BTreeNode<T>* node, T data) {
+        if (node->is_leaf()) {
+            node->dataset.push_back(data);
+            sort(node->dataset.begin(), node->dataset.end());
+            return;
+        }
+
+        if (data <= node->dataset.front()) {
+            loose_insert(node->children.front(), data);
+        }
+        else if (data > node->dataset.back()) {
+            loose_insert(node->children.back(), data);
+        }
+        else {
+            for (size_t i = 0; i < node->dataset.size(); ++i) {
+                if (data <= node->dataset[i]) {
+                    loose_insert(node->children[i], data);
+                    break;
+                }
+            }
+        }
+
+        fix_excess(node);
+    }
+
+    void fix_excess(BTreeNode<T>* node) {
+        if (node->dataset.size() <= 2 * MINIMUM) return;
+
+        if (node == root_ptr) {
+            BTreeNode<T>* new_root = new BTreeNode<T>();
+            new_root->children.push_back(root_ptr);
+            root_ptr->parent = new_root;
+            root_ptr = new_root;
+        }
+
+        BTreeNode<T>* parent = node->parent;
+        if (!parent) return;
+
+        int mid_index = node->dataset.size() / 2;
+        T middle_value = node->dataset[mid_index];
+
+        BTreeNode<T>* right_child = new BTreeNode<T>(parent);
+        right_child->dataset.assign(node->dataset.begin() + mid_index + 1, node->dataset.end());
+        node->dataset.erase(node->dataset.begin() + mid_index, node->dataset.end());
+
+        if (!node->is_leaf()) {
+            right_child->children.assign(node->children.begin() + mid_index + 1, node->children.end());
+            for (auto child : right_child->children) {
+                child->parent = right_child;
+            }
+            node->children.erase(node->children.begin() + mid_index + 1, node->children.end());
+        }
+
+        parent->dataset.push_back(middle_value);
+        sort(parent->dataset.begin(), parent->dataset.end());
+        parent->children.push_back(right_child);
+
+        fix_excess(parent);
+    }
+
+public:
+    Bag(int minimum = 1) : MINIMUM(minimum), root_ptr(nullptr) {}
+
+    void insert(T data) {
+        if (!root_ptr) root_ptr = new BTreeNode<T>();
+        loose_insert(root_ptr, data);
+        fix_excess(root_ptr);
+    }
+
+    void erase_one(T data) {
+        if (!root_ptr) return;
+        loose_erase(root_ptr, data);
+        fix_shortage(root_ptr);
+    }
+
+    void show_contents() const {
+        if (!root_ptr) {
+            cout << "Tree is empty." << endl;
+            return;
+        }
+
+        queue<BTreeNode<T>*> q;
+        q.push(root_ptr);
+
+        while (!q.empty()) {
+            BTreeNode<T>* node = q.front();
+            q.pop();
+
+            cout << "[ ";
+            for (const auto& value : node->dataset) {
+                cout << value << " ";
+            }
+            cout << "] ";
+
+            for (auto child : node->children) {
+                q.push(child);
+            }
+        }
+
+        cout << endl;
     }
 };
 
 int main() {
-    BTree btree;
-    std::string command;
-    int x;
+    Bag<int> bag;
 
-    while (true) {
-        std::cout << "Enter command (insert x / count x / erase x / show / quit): ";
-        std::cin >> command;
-        if (command == "insert") {
-            std::cin >> x;
-            btree.insert(x);
-        }
-        else if (command == "count") {
-            std::cin >> x;
-            std::cout << "Count of " << x << ": " << btree.count(x) << "\n";
-        }
-        else if (command == "erase") {
-            std::cin >> x;
-            if (btree.erase(x)) {
-                std::cout << x << " erased.\n";
-            }
-            else {
-                std::cout << x << " not found.\n";
-            }
-        }
-        else if (command == "show") {
-            btree.show_contents();
-        }
-        else if (command == "quit") {
-            break;
-        }
-        else {
-            std::cout << "Invalid command.\n";
-        }
-    }
+    bag.insert(10);
+    bag.insert(20);
+    bag.insert(5);
+    bag.show_contents();
+
+    bag.erase_one(20);
+    bag.show_contents();
+
+    bag.insert(30);
+    bag.insert(40);
+    bag.erase_one(10);
+    bag.show_contents();
 
     return 0;
 }
-
